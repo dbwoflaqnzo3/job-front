acde9888ab4ee6b8e4662930ad411a4ed611eb21
jianchoi02 <jianchoi02@gmail.com>
2025-01-22 16:58:51 +0900

    feat [#26] : 이메일 인증 및 비밀번호 변경 페이지 기능 구현
    css 작업 요망

--- student/app/loginPage/changePw/page.js
+++ student/app/loginPage/changePw/page.js
@@ -1,6 +1,7 @@
 'use client'
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
 import styles from '../changePw/page.module.css'
@@ -92,10 +93,101 @@ export default function ChangePw({}){
         }
 =======
 export default function ChangePw(){
+=======
+import { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
+import styles from '../changePw/page.module.css'
+>>>>>>> c349648 (feat [#26] : 이메일 인증 및 비밀번호 변경 페이지 기능 구현)
 
-    const handleSubmit = () =>{
+export default function ChangePw({}){
 
+<<<<<<< HEAD
 >>>>>>> 3fe6d00 (feat[#26] : forget pw 이메일 전송, 인증 모달 구현 (timer 제외))
+=======
+    const router = useRouter()
+
+    //비밀번호 관리
+    const [input, setInput] = useState('')
+    const [checkInput, setCheckInput] = useState('')
+    const [condition1, setCondition1] = useState(false) // 길이 체크
+    const [condition2, setCondition2] = useState(false) // 영어,특수문자,숫자 포함 체크
+
+    const [message, setMessage] = useState('')
+    const [modalOpen, setModalOpen] = useState(false)
+
+    const changeInput = (e) => {
+
+        setInput(e.target.value)
+
+    }
+    useEffect(() =>{
+        
+        const regex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[^\w\s]).+$/
+
+        if(input.length < 8) {
+            setCondition1(false)
+        } else{
+            setCondition1(true)
+        }
+
+        if(!regex.test(input)){
+            setCondition2(false)
+        } else{
+            setCondition2(true)
+        }
+        
+    }, [input])
+    
+
+    const handleSubmit = async (e) =>{
+
+        e.preventDefault()
+
+        if(input === ''){
+            setMessage('비밀번호를 입력해 주세요')
+        } else if(checkInput === ''){
+            setMessage('비밀번호를 한번 더 입력해 주세요.')
+        } else if(checkInput !== input){
+            setMessage('비밀번호가 일치하지 않습니다. 다시 작성해 주세요.')
+        }
+
+        try {
+
+            const studentId = localStorage.getItem('id')
+
+            const response = await fetch('http://localhost:8080/student/verifyUpdateStudent', {
+                method: "PATCH",
+                headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({
+                    id: studentId,
+                    password: input
+                  })
+            })
+
+            if(!response.ok){
+                const result = await response.json()
+                console.log(result.message, 'message')
+                throw new Error('요청을 보내는 데 문제가 있습니다.')
+            }
+            
+            if(response.status === 400 || response.status === 404 ){
+                setMessage('비밀번호 변경에 문제가 생겼습니다. 지속적으로 발생할 경우 xxx-xxxx로 문의 바랍니다.')
+                return;
+            }
+
+            const result = await response.json()
+            console.log(result)
+
+        
+            console.log(result)
+            setModalOpen(true)
+
+        } catch(error){
+            console.log(error)
+        }
+>>>>>>> c349648 (feat [#26] : 이메일 인증 및 비밀번호 변경 페이지 기능 구현)
     }
 
     return (
@@ -104,6 +196,9 @@ export default function ChangePw(){
             <form onSubmit={handleSubmit}>
                 <p>새 비밀번호를 작성해주세요</p>
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> c349648 (feat [#26] : 이메일 인증 및 비밀번호 변경 페이지 기능 구현)
                 <input type="password" value={input} onChange={changeInput} placeholder="비밀번호/password"></input> 
                 <ul>
                     {
@@ -113,6 +208,7 @@ export default function ChangePw(){
                         condition2 ? <div></div> : <li style={{color: 'blue'}}>영문/특수문자/숫자가 포함되어야 합니다.</li>
                     }
                 </ul>
+<<<<<<< HEAD
                 <p>한번 더 작성해주세요</p>
                 <input type="password" value={checkInput} onChange={(e)=>setCheckInput(e.target.value)} placeholder="비밀번호/password"></input>
                 {
@@ -134,11 +230,30 @@ export default function ChangePw(){
 =======
                 <input type="password" placeholder="비밀번호/password"></input> 
 
+=======
+>>>>>>> c349648 (feat [#26] : 이메일 인증 및 비밀번호 변경 페이지 기능 구현)
                 <p>한번 더 작성해주세요</p>
-                <input type="password" placeholder="비밀번호/password"></input>
+                <input type="password" value={checkInput} onChange={(e)=>setCheckInput(e.target.value)} placeholder="비밀번호/password"></input>
+                {
+                    message && <p>{message}</p>
+                }
 
                 <button type="submit">변경하기</button>
+<<<<<<< HEAD
 >>>>>>> 3fe6d00 (feat[#26] : forget pw 이메일 전송, 인증 모달 구현 (timer 제외))
+=======
+                {
+                    modalOpen && 
+
+                    <div className={styles.modalOverlay}> 
+                        <div className={styles.modalContainer}>
+                            <h2 className={styles.modalTitle} >비밀번호가 변경되었습니다.</h2>
+                            <button className={styles.buttonCancel} onClick={() => router.push('../loginPage')}>로그인 페이지로 돌아가기</button>
+                        </div>
+                        
+                    </div>
+                }
+>>>>>>> c349648 (feat [#26] : 이메일 인증 및 비밀번호 변경 페이지 기능 구현)
             </form>
         </div>     
     )
--- student/app/loginPage/forgetPw/page.js
+++ student/app/loginPage/forgetPw/page.js
@@ -17,10 +17,18 @@ export default function ForgetPw() {
   const [modalOpen, setModalOpen] = useState(false)
   const [inputVerificationNum, setInputVerificationNum] = useState('')
   const maxLength = 8 //인증 번호 최대 숫자 자리
+  const [modalMessage, setModalMessage] = useState('')
 
   //인증 timer
+<<<<<<< HEAD
   const initialTime = 180
   const [timeLeft, setTimeLeft] = useState(0); // 남은 시간 (초 단위)
+=======
+  const [timeLeft, setTimeLeft] = useState(0); // timer 남은 시간 (초 단위)
+  const [isExpiredTime, setIsExpiredTime] = useState(false)
+  const [expireDate, setExpireDate] = useState(null)
+  const [checkDuplication, setCheckDuplication] = useState(false)
+>>>>>>> c349648 (feat [#26] : 이메일 인증 및 비밀번호 변경 페이지 기능 구현)
 
   const router = useRouter()
 
@@ -61,9 +69,15 @@ export default function ForgetPw() {
 
   //변경하기 버튼 클릭 
   const handleSubmit = async (e) => {
+    
     e.preventDefault()
+<<<<<<< HEAD
 
 >>>>>>> 3fe6d00 (feat[#26] : forget pw 이메일 전송, 인증 모달 구현 (timer 제외))
+=======
+    setModalOpen(false)
+  
+>>>>>>> c349648 (feat [#26] : 이메일 인증 및 비밀번호 변경 페이지 기능 구현)
     //이메일 형식이 틀렸을 경우
     if (!emailId || !selectedOption) {
       setMessage("이메일을 올바르게 입력해주세요.")
@@ -90,49 +104,36 @@ export default function ForgetPw() {
           return
         }
 
-        setIsExpoiredTime(false)
+        setIsExpiredTime(false)
+
+        const { result, resExpireDate } = await response.json() //생성된 ID, 만료 시간 불러오기
+        console.log(resExpireDate, "만료 시간")
+
+        const now = new Date(Date.now()) // 현재 시간 설정
+        const expireDate = new Date(resExpireDate) // 만료 시간 설정 
+        setExpireDate(expireDate)
+        setVerificationResponseId(result) // studentId 저장 
 
         // 중복 전송 시
         if(response.status === 400){
 
-          const now = new Date(Date.now())
-
           // 인증 요청이 만료됐을 때
-          if(now > initialTime){
-            sessionStorage.setItem('expireDate', null)
-            setIsExpoiredTime(true)
+          if(now > result){
+            setIsExpiredTime(true)
           }
           else{
-            const t = initialTime - now 
-            setTimeLeft(Math.floor(t/1000))
+            setTimeLeft(Math.floor((expireDate - now) / 1000))
             setCheckDuplication(true)
           }
-
         } else { // 정상적인 인증 요청
 
-          // if (!response.ok) {
-          //   setError(result.message || '이메일 인증 실패');
-          //   console.log(error)
-          // }
-
-          const { result } = await response.json()
-
-
-          setVerificationResponseId(result)
-  
-          //이메일 전송 성공
-          setMessage(`인증 메일이 ${fullEmail}로 전송되었습니다.`)
-  
-          // 처음으로 이메일 인증할 때
-          const expireDate = new Date(Date.now() + 3 * 60 * 1000)
-          sessionStorage.setItem('expireDate', expireDate)
-          setInitialTime(expireDate)
-          setTimeLeft(180)
+          // 이메일 인증 기본 시간 설정
+          setTimeLeft(Math.floor((expireDate - now) / 1000))
 
         }
-      
-      setMessage(null)
-      setModalOpen(true)
+
+        setMessage(null)
+        setModalOpen(true)
 
     } catch (error) {
       console.error(error)
@@ -146,14 +147,15 @@ export default function ForgetPw() {
     if (timeLeft > 0) {
       const interval = setInterval(() => {
         setTimeLeft((prev) => {
+          const now = new Date(Date.now())
           if (prev <= 1) {
             clearInterval(interval)
-            setIsExpoiredTime(true)
+            setIsExpiredTime(true)
             return 0
           }
-          return prev - 1
+          return Math.floor((expireDate - now) / 1000)
         });
-      }, 1000)
+      }, 100)
 
       return () => clearInterval(interval) // 컴포넌트 언마운트 시 클리어
     }
@@ -178,18 +180,66 @@ export default function ForgetPw() {
     }
   }
 
+<<<<<<< HEAD
   // 함수 명 변경 요망망
   const endWritingStudy = () => {
 
+=======
+  // 
+  const verifyId = async (e) => {
+
+    e.preventDefault()
+
+    try{
+
+      console.log(verificationResponseId, inputVerificationNum, "인증 아이디 및 입력 코드")
+      
+      if(inputVerificationNum === ''){
+        setModalMessage("인증 번호를 입력해 주세요.");
+        return;
+      }
+
+       const response = await fetch('http://localhost:8080/verifyService/verify', {
+        method: "PATCH",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          _id: verificationResponseId,
+          code: inputVerificationNum
+        })
+      })
+
+      if(response.status === 404){
+        setModalMessage("만료된 요청입니다. 요청을 재전송해주세요.")
+      }
+
+      const verifyInfo = await response.json()
+      
+      if(verifyInfo.status === 2){
+        setModalMessage("만료된 요청입니다. 요청을 재전송해주세요.")
+        return
+      }else if(verifyInfo.status === 0){
+        setModalMessage("잘못된 인증번호입니다.")
+        return
+      }else if(verifyInfo.status === 1){
+        localStorage.setItem("id", verifyInfo.studentId)
+        router.push('/../loginPage/changePw')
+
+      }
+    } catch (error) {
+      console.error(error)
+    }
+>>>>>>> c349648 (feat [#26] : 이메일 인증 및 비밀번호 변경 페이지 기능 구현)
   }
 
   return (
     <div className={styles.container}>
-      <h1>비밀번호 변경하기</h1>
+      <h1 className={styles.title}>비밀번호 변경하기</h1>
       <form onSubmit={handleSubmit}>
         <div className={styles.inputGroup}>
           <div>
-            <p>인증 가능한 이메일 주소를 입력하세요.</p>
+            <p className={styles.inputTitle} >인증 가능한 이메일 주소를 입력하세요.</p>
           </div>
           <div className={styles.emailBox}>
             <input
@@ -235,6 +285,10 @@ export default function ForgetPw() {
                 // maxLength={maxLength}
                 onChange={(e) => setInputVerficationNum(e.target.value)}
               />
+<<<<<<< HEAD
+=======
+              {modalMessage && <p className={styles.message}>{modalMessage}</p>}
+>>>>>>> c349648 (feat [#26] : 이메일 인증 및 비밀번호 변경 페이지 기능 구현)
               {
                 timeLeft > 0 && (
                   <p>{formatTime(timeLeft)}</p>
