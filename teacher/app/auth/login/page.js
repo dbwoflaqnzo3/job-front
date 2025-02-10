fcd340721f61aef73ba18f5e4e5c1e2593db7ed2
seungjoonH <hsj6831@gmail.com>
2025-02-10 20:53:57 +0900

    comp[Closed #43] 로그인 페이지 구현

--- teacher/app/auth/login/page.js
+++ teacher/app/auth/login/page.js
@@ -1,13 +1,41 @@
-'use client'
+"use client";
 
-import { Row, Column } from "@/app/widgets/structure/Grid";
-import Image from "next/image";
-import Logo from "@/public/assets/images/logo.svg";
+import { useState, useEffect } from "react";
+import Card from "@/app/components/ui/Card";
+import LoginForm from "@/app/components/form/login";
+import styles from "./page.module.css";
 
 export default function Login() {
+  const width = 684;
+  const height = `calc(100vh - 96px)`;
+  const paddingHorizontal = 142;
+  const paddingVertical = 90;
+
+  const [isWideScreen, setIsWideScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsWideScreen(window.innerWidth >= 1368);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <Column>
-      <Logo />
-    </Column>
+    <div className={`page ${styles["login"]}`}>
+      <div className={`${styles["align"]} ${isWideScreen ? styles["right"] : styles["center"]}`}>
+        <Card 
+          width={width}
+          height={height}
+          paddingHorizontal={paddingHorizontal}
+          paddingVertical={paddingVertical}
+        >
+          <LoginForm />
+        </Card>
+      </div>
+    </div>
   );
 }
