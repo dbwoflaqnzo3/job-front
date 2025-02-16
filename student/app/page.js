fa30cbfacd2dff3415f61dd3dc21ae780d6443b8
반대준 <67678049+daejun4582@users.noreply.github.com>
2025-02-16 19:10:24 +0900

    feat[#44] : component 적용 오류 해결 중

--- student/app/page.js
+++ student/app/page.js
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import styles from "./page.module.css";
+<<<<<<< HEAD
 import { useRouter } from "next/navigation";
 import { NavGroup, NavTitle, NavIcon, NavItem } from "@/app/components/layout/Nav";
 
@@ -30,13 +31,42 @@ export function PageLayout({ children }) {
         {children}
       </div>
     </div>
+=======
+import { NavGroup, NavTitle, NavIcon, NavItem } from "@/app/components/layout/Nav";
+
+function pageLayout() {
+  return (
+    <NavGroup theme="primary">
+      <NavTitle title="A" onClick={() => {}} />
+      <NavTitle title="B" onClick={() => {}} />
+      <NavTitle title="C">
+        <NavItem text="C-1" onClick={() => {}} />
+        <NavItem text="C-2" onClick={() => {}} />
+      </NavTitle>
+      <NavIcon icon="help" onClick={() => {}} />
+      <NavIcon icon="profile">
+        <NavItem text="1" onClick={() => {}} />
+        <NavItem text="2" onClick={() => {}} />
+        <NavItem text="3" onClick={() => {}} />
+        <NavItem text="4" textColor="red-3" onClick={() => {}} />
+      </NavIcon>
+    </NavGroup>
+>>>>>>> 3ee5730 (feat[#44] : component 적용 오류 해결 중)
   );
 }
 
 export default function Home() {
   return (
+<<<<<<< HEAD
     <PageLayout>
       <div>Welcome to JOB Student Page</div>
     </PageLayout>
+=======
+    <div>
+      {
+        pageLayout()
+      }
+    </div>
+>>>>>>> 3ee5730 (feat[#44] : component 적용 오류 해결 중)
   );
 }
--- student/app/widgets/structure/Grid.js
+++ student/app/widgets/structure/Grid.js
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 import { addRequestMeta } from "next/dist/server/request-meta";
+=======
+>>>>>>> 3ee5730 (feat[#44] : component 적용 오류 해결 중)
 import styles from "./grid.module.css";
 
 export function Grid({ 
