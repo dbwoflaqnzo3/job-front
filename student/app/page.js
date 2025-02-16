3ee5730b54cea87a5ed02f5deaac656ca8fab329
반대준 <67678049+daejun4582@users.noreply.github.com>
2025-02-16 19:10:24 +0900

    feat[#44] : component 적용 오류 해결 중

--- student/app/page.js
+++ student/app/page.js
@@ -1,8 +1,35 @@
+'use client'
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { NavGroup, NavTitle, NavIcon, NavItem } from "@/app/components/layout/Nav";
+
+function pageLayout() {
+  return (
+    <NavGroup theme="primary">
+      <NavTitle title="A" onClick={() => {}} />
+      <NavTitle title="B" onClick={() => {}} />
+      <NavTitle title="C">
+        <NavItem text="C-1" onClick={() => {}} />
+        <NavItem text="C-2" onClick={() => {}} />
+      </NavTitle>
+      <NavIcon icon="help" onClick={() => {}} />
+      <NavIcon icon="profile">
+        <NavItem text="1" onClick={() => {}} />
+        <NavItem text="2" onClick={() => {}} />
+        <NavItem text="3" onClick={() => {}} />
+        <NavItem text="4" textColor="red-3" onClick={() => {}} />
+      </NavIcon>
+    </NavGroup>
+  );
+}
 
 export default function Home() {
   return (
-    <div> Welcome to JOB Project</div>
+    <div>
+      {
+        pageLayout()
+      }
+    </div>
   );
 }
